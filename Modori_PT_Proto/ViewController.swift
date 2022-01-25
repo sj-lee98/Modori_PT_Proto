06116c849704d87e422a6fc0dbb38ab5051a9c14
sj-lee98 <denny00123@naver.com>
2022-01-25 10:24:43 +0900

    Initial Commit

---
+++ Modori_PT_Proto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modori_PT_Proto
+//
+//  Created by 이성주 on 2022/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
